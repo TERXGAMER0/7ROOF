a51eca372a3307fdfd93d8a561c13bcea7dabfed
TERXGAMER0 <TerXGamer123@gmail.com>
2025-04-01 17:01:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 // إعداد محرك القوالب
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public'));
 
 // تقديم الملفات الثابتة من مجلد 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 // التعامل مع الطلبات إلى الصفحة الرئيسية
 app.get('/', (req, res) => {
